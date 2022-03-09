1bbc4142f047d3ddc6198cf10e84045f9ebd9db1
Felipe Gazcon <pipegazcon123@gmail.com>
2022-03-09 16:41:57 -0300

    Guardado 1 9 de Marzo

---
+++ src/Componentes/ItemList/Items/ItemUno.js
@@ -0,0 +1,10 @@
+function ItemUno(props) {
+  return (
+    <li className="ItemUno">
+        <p>Nombre= {props.nombre} </p>
+        <p>Descripcion= {props.descripcion} </p>
+    </li>
+  );
+}
+
+export default ItemUno;
