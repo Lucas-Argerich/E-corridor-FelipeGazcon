f730c7f7e6011559b149b87b825a5d0552d17e0b
Felipe Gazcon <pipegazcon123@gmail.com>
2022-03-10 18:13:14 -0300

    Guardado 3 10 de Marzo

--- src/Componentes/ItemList/Items/ItemUno.js
+++ src/Componentes/ItemList/Items/ItemUno.js
@@ -2,31 +2,34 @@ import React, {useState, useEffect} from "react";
 
 function ItemUno(props) {
 
-  const [numeroActual, cambiarnumeroActual] = React.useState(0)
+  const [numeroActual, cambiarnumeroActual] = React.useState(0);
+  const [stockActual, cambiarstockActual] = React.useState(8)
 
-  const stock1 = Math.random()*10;
-  console.log(stock1);
+  console.log(stockActual);
 
   return (
     <li className="ItemUno">
         <p>Nombre: {props.nombre} </p>
         <p>Descripcion: {props.descripcion} </p>
-        <p>Stock: {stock1}</p>
+        <p>Stock: {stockActual}</p>
         <button onClick={() => {
-          if(numeroActual + 1 > stock1){
-            console.log("Imposible la compra item 2");
+          if(numeroActual >= stockActual){
+            console.log("Imposible la compra item ");
           }else{
             console.log("Click")
             cambiarnumeroActual(numeroActual+1)
+            cambiarstockActual(stockActual-1)
           }
         }}>+1</button>
         <p>{numeroActual}</p>
         <button onClick={() => {
           if(numeroActual === 0){
-            console.log("Imposible la compra item 1");
+            console.log("Imposible la compra item 2");
           }else{
             console.log("Click")
             cambiarnumeroActual(numeroActual-1)
+            cambiarstockActual(stockActual+1)
+            return stockActual 
           }
         }}>-1</button>
     </li>
